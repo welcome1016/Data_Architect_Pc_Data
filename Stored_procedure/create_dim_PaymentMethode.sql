56802517445af42362178a795b8930f86805a2c7
welcome shongwe <welcomeshongwe1016@gmail.com>
2026-05-07 20:59:18 +0200

    creating stored procedure for dim pyamentMethod

---
+++ Stored_procedure/create_dim_PaymentMethode.sql
@@ -0,0 +1,15 @@
+
+CREATE PROCEDURE sp_create_dim_payment_method
+
+AS
+BEGIN
+drop table if exists [dbo].[Dim_Payment_Method]
+CREATE TABLE [Pc_Data_Staging]. [dbo].[Dim_Payment_Method](
+	[Payment_Method_ID] [int] IDENTITY(1,1) Primary key,
+	[Payment_Method] [nvarchar](50) NOT NULL
+) 
+insert into [Pc_Data_Staging].[dbo].[Dim_Payment_Method] ([Payment_Method])
+select distinct [Payment_Method]
+from [Pc_Data_Staging].[dbo].[pc_data _RawData]
+END;
+
