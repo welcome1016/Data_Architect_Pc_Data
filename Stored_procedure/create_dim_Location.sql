ee158cda5ef06daa1a39c09b49be5c6ee78e3825
welcome shongwe <welcomeshongwe1016@gmail.com>
2026-05-07 20:53:11 +0200

    created a strore procedure for dim_location

---
+++ Stored_procedure/create_dim_Location.sql
@@ -0,0 +1,19 @@
+
+
+
+
+CREATE PROCEDURE sp_create_dim_Location
+ 
+AS
+BEGIN
+
+CREATE TABLE [Pc_Data_Staging].[dbo].[dim_Location](
+	[locationID] [int] IDENTITY(1,1) primary key,
+	[Continent] [nvarchar](50) NOT NULL,
+	[Country_or_State] [nvarchar](50) NOT NULL,
+	[Province_or_City] [nvarchar](100) NOT NULL
+)
+insert into [Pc_Data_Staging].[dbo].[dim_Location] ( [Continent],[Country_or_State] ,[Province_or_City] )
+select distinct [Continent],[Country_or_State] ,[Province_or_City]
+from  [Pc_Data_Staging].[dbo].[pc_data _RawData]
+END;
