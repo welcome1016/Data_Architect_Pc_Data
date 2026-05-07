f488c6da738511591b095239c4e82fd99b342eaf
welcome shongwe <welcomeshongwe1016@gmail.com>
2026-05-07 21:02:29 +0200

    creating stored procedure for dim properties

---
+++ Stored_procedure/create_dim_properties.sql
@@ -0,0 +1,18 @@
+
+CREATE PROCEDURE sp_create_dim_pc_properties
+ 
+AS
+BEGIN
+drop table if exists [dbo].[Dim_Pc_Properties]
+CREATE TABLE[Pc_Data_Staging].[dbo].[Dim_Pc_Properties](
+	[Properies_ID] [int] IDENTITY(1,1) Primary key,
+	[PC_Make] [nvarchar](50) NOT NULL,
+	[PC_Model] [nvarchar](50) NOT NULL,
+	[Storage_Type] [nvarchar](50) NOT NULL,
+	[RAM] [nvarchar](50) NOT NULL,
+	[Storage_Capacity] [nvarchar](50) NOT NULL
+) 
+insert into [Pc_Data_Staging].[dbo].[Dim_Pc_Properties] ([PC_Make], [PC_Model], [Storage_Type], [RAM], [Storage_Capacity])
+select distinct [PC_Make], [PC_Model], [Storage_Type], [RAM], [Storage_Capacity]
+from [Pc_Data_Staging].[dbo].[pc_data _RawData]
+END;
