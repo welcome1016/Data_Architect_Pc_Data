ea3d1136e8c57e62cfed50415808ea5db38f0cb0
welcome shongwe <welcomeshongwe1016@gmail.com>
2026-05-07 21:12:04 +0200

    creating stored procedure for dim shop

---
+++ Stored_procedure/create_dim_shop.sql
@@ -0,0 +1,14 @@
+
+CREATE PROCEDURE sp_create_dim_shop
+AS
+BEGIN
+drop table if exists [dbo].[Dim_Shop]
+CREATE TABLE [Pc_Data_Staging].[dbo].[Dim_Shop](
+	[Shop_ID] [int] IDENTITY(1,1) Primary key,
+	[Shop_Name] [nvarchar](50) NOT NULL,
+	[Shop_Age] [nvarchar](50) NOT NULL
+) 
+insert into [Pc_Data_Staging].[dbo].[Dim_Shop] ([Shop_Name], [Shop_Age])
+select distinct [Shop_Name], [Shop_Age]
+from [Pc_Data_Staging].[dbo].[pc_data _RawData]
+END;
